85987cf761940e9831def179a1e55278ee447210
UserBruno <lorenzoeliasb@gmail.com>
2025-06-26 10:44:49 +0200

    views

--- WebServer_Tickets/WebTickets/backend/views.py
+++ WebServer_Tickets/WebTickets/backend/views.py
@@ -15,3 +15,41 @@ from backend.models import (
     Trenostazione,
     Utente
 )
+
+# Logics
+
+# home logic
+class HomeLogic(View):
+
+    TEMPLATE = 'Home.html'
+
+    def get(self, request: HttpRequest):
+        return render(request, self.TEMPLATE)
+    #
+    def post(self, request: HttpRequest):
+
+        # user in session
+        user_id = request.session.get('user_id')
+
+        # check user in session
+        if user_id != None:
+            print(f"User logged in session is {user_id}")
+
+        action = request.POST.get('action')
+        try:
+            if action == "buysubscription":
+                return self.buysubscription(request)
+            elif action == "buyticket":
+                return self.buyticket(request)
+            else:
+                print("Errore. Gestione action form html")
+        except Exception as e:
+            print(f"Errore. try-except action form")
+    #
+    def buysubscription(self, request: HttpRequest):
+        print("")
+    #
+    def buyticket(self, request: HttpRequest):
+        print("")
+    #
+#
