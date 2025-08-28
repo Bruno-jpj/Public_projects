a0ee40fa6ef6b97073bb66bf84c49c229b1c6877
Bruno-jpj <82368758+Bruno-jpj@users.noreply.github.com>
2025-08-28 19:28:32 +0200

    Delete SyncSyntax-master directory

--- SyncSyntax-master/SyncSyntax/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SyncSyntax.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
